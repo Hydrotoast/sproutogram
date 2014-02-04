94fd36dd75f34df981036753290d6838e230dfb7
Gio Cielo <gccielo@gmail.com>
2014-02-04 13:11:05 -0800

    Refactor database scripts to adhere to PEP8

--- src/db/DataDumpToCSV.py
+++ src/db/DataDumpToCSV.py
@@ -3,32 +3,34 @@ import sqlite3
 
 from operator import itemgetter
 
-def main():
-	with sqlite3.connect('extractions.db') as conn:
-		cur = conn.cursor()
-		cur.execute('SELECT DISTINCT method FROM feature')
-		methods = [m[0] for m in cur.fetchall()]
-		data = []
-		for method in methods:
-			cur.execute("SELECT sprout_count, branching_count, sprout_maximum \
-				FROM feature \
-				WHERE method = ? \
-				ORDER BY filename",
-				[method])
-			row = cur.fetchall()
-			data.append(row)
-		with open('sprout_data.csv', 'w') as fh:
-			writer = csv.writer(fh)
-			for i, method in enumerate(methods):
-				writer.writerow([method] +
-					list(map(itemgetter(0), data[i])))
-		with open('branching_data.csv', 'w') as fh:
-			writer = csv.writer(fh)
-			for i, method in enumerate(methods):
-				writer.writerow([method] + list(map(itemgetter(1), data[i])))
-		with open('sprout_maximum_data.csv', 'w') as fh:
-			writer = csv.writer(fh)
-			for i, method in enumerate(methods):
-				writer.writerow([method] + list(map(itemgetter(2), data[i])))
 
-main()
+def main():
+    with sqlite3.connect('extractions.db') as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT DISTINCT method FROM feature')
+        methods = [m[0] for m in cur.fetchall()]
+        data = []
+        for method in methods:
+            cur.execute("SELECT sprout_count, branching_count, sprout_maximum \
+                FROM feature \
+                WHERE method = ? \
+                ORDER BY filename",
+                [method])
+            row = cur.fetchall()
+            data.append(row)
+        with open('sprout_data.csv', 'w') as fh:
+            writer = csv.writer(fh)
+            for i, method in enumerate(methods):
+                writer.writerow([method] +
+                    list(map(itemgetter(0), data[i])))
+        with open('branching_data.csv', 'w') as fh:
+            writer = csv.writer(fh)
+            for i, method in enumerate(methods):
+                writer.writerow([method] + list(map(itemgetter(1), data[i])))
+        with open('sprout_maximum_data.csv', 'w') as fh:
+            writer = csv.writer(fh)
+            for i, method in enumerate(methods):
+                writer.writerow([method] + list(map(itemgetter(2), data[i])))
+
+if __name__ == '__main__':
+    main()
--- src/db/OverviewReport.py
+++ src/db/OverviewReport.py
@@ -1,13 +1,14 @@
 import math
 import sqlite3 as sqlite
 
+
 def main():
-	with sqlite.connect('extractions.db') as conn:
-		cur = conn.cursor()
-		cur.execute('SELECT * FROM overview')
-		rows = cur.fetchall()
-		for row in rows:
-			print '{0:s}, {1:.2f}, {2:.2f}, {3:.2f}'.format(row[0], math.sqrt(row[1]), math.sqrt(row[2]), math.sqrt(row[3]))
+    with sqlite.connect('extractions.db') as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM overview')
+        rows = cur.fetchall()
+        for row in rows:
+            print '{0:s}, {1:.2f}, {2:.2f}, {3:.2f}'.format(row[0], math.sqrt(row[1]), math.sqrt(row[2]), math.sqrt(row[3]))
 
 if __name__ == '__main__':
-	main()
+    main()
