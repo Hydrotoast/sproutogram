22267269198f0fce9bb4943488a42b6a786bc151
Gio Cielo <gccielo@gmail.com>
2014-10-08 01:07:01 -0700

    Add build script for installing dependencies

---
+++ bin/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#install pip for installing libraries
+sudo apt-get install pip
+
+# install simplecv library and its dependencies
+sudo apt-get install python-pygame
+sudo pip install simplecv
