2ba31a1b2de6c6622a2aefeb84138cac61a3619a
Gio Cielo <gccielo@gmail.com>
2014-10-08 16:32:18 -0700

    Modify build script to install simplecv correctly

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,10 @@
 #install pip for installing libraries
 sudo apt-get install pip
 
+# install scipy for scientific computations
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+
 # install simplecv library and its dependencies
 sudo apt-get install python-pygame
-sudo pip install simplecv
+sudo apt-get install libopencv python-opencv
+sudo pip install sqlalchemy simplecv
